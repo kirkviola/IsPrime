bbd5bed0710e4aa5b846149ef5b9d673d13198ee
Matthew Kirkendall <kirk.viola@gmail.com>
2021-09-24 11:26:26 -0400

    Add project files.

---
+++ IsPrime/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         try
+            {
+                IsPrime();
+            }         
+         catch (System.FormatException)
+            {
+                Console.WriteLine("Please enter an integer next time bub.");
+            }
+            
+        }
+        
+        static void IsPrime()
+        {
+            Console.WriteLine("Enter an integer.");
+
+            var EnteredString = Console.ReadLine();
+            var TestNumber = 2;
+            var EnteredNumber = Convert.ToInt32(EnteredString);
+            var NotPrime = true;
+            while (TestNumber <= EnteredNumber / 2)
+            {
+                if (EnteredNumber % TestNumber == 0)
+                {
+                    NotPrime = false;
+                    break;
+                }
+
+                TestNumber += 1;
+
+            }
+            if (NotPrime == true)
+            {
+                Console.WriteLine($"{EnteredNumber} is prime!");
+            }
+            else
+            {
+                Console.WriteLine($"{EnteredNumber} is not prime.");
+            }
+        }
+    }
+}
